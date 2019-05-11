963285a469aea620efc614e892e168ef1a2cdbdb
Laurent <laurent.lesle@microsoft.com>
2019-05-11 22:59:05 +0800

    Fixing the subnets creation

--- subnet.tf
+++ subnet.tf
@@ -4,8 +4,8 @@ locals {
 
 resource "azurerm_subnet" "subnets" {
     count                   = "${length(var.subnets)}"
-    name                    = "${var.subnets[element(local.subnet_list, count.index)]}"
+    name                    = "${element(local.subnet_list, count.index)}"
     resource_group_name     = "${var.resource_group_name}"
     virtual_network_name    = "${azurerm_virtual_network.vnet.name}"
-    address_prefix          = "${lookup(var.vnet, var.subnets[element(local.subnet_list, count.index)])}"
+    address_prefix          = "${lookup(var.subnets, var.subnets[element(local.subnet_list, count.index)])}"
 }
