3b3f3af67fd53e0a6fa5beb59c70dd6d3550641c
Laurent <laurent.lesle@microsoft.com>
2019-05-31 00:00:49 +0800

    Updating dns module to 2.0-beta1

--- private_dns.tf
+++ private_dns.tf
@@ -1,6 +1,6 @@
 # Internal DNS used to host the applications published through the ingress controller
 module "azure_dns_internal" {
-  source = "git://github.com/LaurentLesle/azure_terraform_blueprint_modules_azure_dns.git?ref=v1.0"
+  source = "git://github.com/LaurentLesle/azure_terraform_blueprint_modules_azure_dns.git?ref=v2.0-beta1"
 
   resource_group_name            = var.resource_group_name
   dns_zone                       = var.dns_zone
