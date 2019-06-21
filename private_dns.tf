cbaa0a38fddaac487a8b15dfdf21a3848f357952
Laurent <laurent.lesle@microsoft.com>
2019-06-21 18:38:37 +0800

    Adding var.prefix in the subdomain if set

--- private_dns.tf
+++ private_dns.tf
@@ -4,7 +4,10 @@ module "azure_dns_internal" {
 
   resource_group_name            = var.resource_group_name
   dns_zone                       = var.dns_zone
-  subdomain                      = "${var.location}."
+  subdomain                      = local.subdomain
   resolution_virtual_network_ids = [azurerm_virtual_network.vnet.id]
 }
 
+locals {
+    subdomain = var.prefix == "" ? "${var.location}." : "${var.prefix}.${var.location}."
+}
